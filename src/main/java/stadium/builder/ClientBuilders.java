4e008573467eb2b1877121aa51f1a0cc439711c4
Denis Kruminsh <ipsaer@gmail.com>
2017-11-06 18:53:45 +0300

    Total price added to @GetMapping("/client/{id}/buys")

--- src/main/java/stadium/builder/ClientBuilders.java
+++ src/main/java/stadium/builder/ClientBuilders.java
@@ -10,9 +10,16 @@ import java.util.Map;
 public class ClientBuilders {
     public static void appendSellEntres(Sell sell, List<SellEntre> sellEntres, List<Map<String, Object>> data) {
         List<Map<String, Object>> mappedSellEntres = new ArrayList<>();
-        sellEntres.forEach(item -> mappedSellEntres.add(item.getMap()));
+        Double totalPrice = 0.0;
+        for (SellEntre item: sellEntres) {
+            mappedSellEntres.add(item.getMap());
+            if(item.getProductPrice()!=null&&item.getQuantity()!=null)
+                totalPrice+=item.getProductPrice()*item.getQuantity();
+        }
         Map<String, Object> mappedSell = sell.getMap();
         mappedSell.put("sellEntres", mappedSellEntres);
+        mappedSell.put("totalPrice", totalPrice);
+
         data.add(mappedSell);
     }
     public static void setSellsSize(List<Sell> sells, Map<String, Object> data) {
